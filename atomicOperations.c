d86cb4026280511c6f5c0e315d2acacf3f38cf5f
Adam Rawashdeh <adamrawashdeh@Adams-MacBook-Pro.local>
2026-04-04 14:42:01 -0400

    Update atomicOperations.c

--- atomicOperations.c
+++ atomicOperations.c
@@ -1,11 +1,7 @@
-//https://learn.microsoft.com/en-us/cpp/parallel/openmp/reference/openmp-directives?view=msvc-170&utm_source=chatgpt.com
-//https://www.intel.com/content/www/us/en/docs/advisor/user-guide/2024-1/openmp-reduction-operations.html?utm_source=chatgpt.com
-
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 
 int main(int argc, const char *argv[]) {
     if (argc != 4) {
@@ -16,7 +12,10 @@ int main(int argc, const char *argv[]) {
     long long iterations = atoll(argv[1]);
     const char* mode;
 
-    if (strcmp(argv[2], "atomic") == 0) {
+    if (strcmp(argv[2], "seq") == 0) {
+        mode = "seq";
+    }
+    else if (strcmp(argv[2], "atomic") == 0) {
         mode = "atomic";
     }
     else if (strcmp(argv[2], "critical") == 0) {
@@ -26,7 +25,7 @@ int main(int argc, const char *argv[]) {
         mode = "reduction";
     }
     else {
-        fprintf(stderr, "Invalid mode");
+        fprintf(stderr, "Invalid mode\n");
         return EXIT_FAILURE;
     }
 
@@ -40,40 +39,50 @@ int main(int argc, const char *argv[]) {
     omp_set_num_threads(threads);
 
     long long counter = 0;
+
     double start = omp_get_wtime();
 
-    if (strcmp(mode, "reduction") == 0) {
+    if (strcmp(mode, "seq") == 0) {
+        for (long long i = 0; i < iterations; i++) {
+            counter += (i & 1);
+        }
+    }
+    else if (strcmp(mode, "reduction") == 0) {
         long long sum = 0;
 
         #pragma omp parallel for reduction(+:sum) schedule(static)
         for (long long i = 0; i < iterations; i++) {
-            sum += 1;
+            sum += (i & 1);
         }
 
         counter = sum;
-    } else if (strcmp(mode, "atomic") == 0) {
-        #pragma omp parallel for schedule(static) 
+    }
+    else if (strcmp(mode, "atomic") == 0) {
+        #pragma omp parallel for schedule(static)
         for (long long i = 0; i < iterations; i++) {
+            long long val = (i & 1);
             #pragma omp atomic
-            counter += 1;
+            counter += val;
         }
-    } else {
-        #pragma omp parallel for schedule(static)  
-            for (long long i = 0; i < iterations; i++) {
-                #pragma omp critical
-                {
-                    counter += 1;
-                }
+    }
+    else {
+        #pragma omp parallel for schedule(static)
+        for (long long i = 0; i < iterations; i++) {
+            long long val = (i & 1);
+            #pragma omp critical
+            {
+                counter += val;
             }
+        }
     }
 
     double end = omp_get_wtime();
 
-    printf("mode=%s\n", argv[2]);
+    printf("mode=%s\n", mode);
     printf("threads=%d\n", threads);
     printf("iterations=%lld\n", iterations);
     printf("result=%lld\n", counter);
     printf("time=%f\n", end - start);
-    
+
     return 0;
 }
